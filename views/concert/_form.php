012901fbcd70cce99e6dc48b8a587b794d5fba40
Hristo Hristov <koredalin@gmail.com>
2018-11-17 21:31:06 +0200

    Image upload init

--- views/concert/_form.php
+++ views/concert/_form.php
@@ -5,12 +5,14 @@ use yii\widgets\ActiveForm;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Concert */
+/* @var $bandModel app\models\Band */
+/* @var $countryModel app\models\country */
 /* @var $form yii\widgets\ActiveForm */
 ?>
 
 <div class="concert-form">
 
-    <?php $form = ActiveForm::begin(); ?>
+    <?php $form = ActiveForm::begin(['options' => ['enctype' => 'multipart/form-data']]); ?>
 
     <?= $form->field($model, 'date')->textInput() ?>
 
@@ -22,9 +24,23 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'description')->textarea(['rows' => 6]) ?>
 
-    <?= $form->field($model, 'created_at')->textInput() ?>
+    
+    <?php
+    // https://www.yiiframework.com/doc/guide/2.0/en/input-file-upload
+    // echo $imageForm = ActiveForm::begin(['options' => ['enctype' => 'multipart/form-data']]);
+    ?>
 
-    <?= $form->field($model, 'updated_at')->textInput() ?>
+        <?php echo $imageForm->field($bandModel, 'imageFile')->fileInput(); ?>
+
+        <button>Submit</button>
+
+    <?php // ActiveForm::end() ?>
+
+    <?php 
+    // Not used inputs.
+    // echo $form->field($model, 'created_at')->textInput();
+    // echo $form->field($model, 'updated_at')->textInput();
+    ?>
 
     <div class="form-group">
         <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-success']) ?>
