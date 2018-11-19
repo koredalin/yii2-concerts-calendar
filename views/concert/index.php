4a5f0b1b6543b8128143e9094fcbecb6daab4463
Hristo Hristov <koredalin@gmail.com>
2018-11-20 01:34:59 +0200

    Datepicker Search Date init

--- views/concert/index.php
+++ views/concert/index.php
@@ -3,6 +3,8 @@
 use yii\helpers\Html;
 use yii\grid\GridView;
 use yii\widgets\Pjax;
+use yii\jui\DatePicker;
+
 /* @var $this yii\web\View */
 /* @var $searchModel app\models\search\ConcertSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
@@ -20,7 +22,17 @@ $this->params['breadcrumbs'][] = $this->title;
         <?= Html::a(Yii::t('app', 'Create Concert'), ['create'], ['class' => 'btn btn-success']) ?>
     </p>
 
-    <?= GridView::widget([
+    <?php
+    $dateFilter = '<div class="inner-addon right-addon">'.PHP_EOL;
+    $dateFilter .= '<i class="glyphicon glyphicon-calendar"></i>'.PHP_EOL;
+    $dateFilter .= DatePicker::widget([
+            'model' => $searchModel,
+            'attribute' => 'date',
+            'dateFormat' => 'yyyy-MM-dd',
+            'options' => ['class' => 'form-control'],
+        ]).PHP_EOL;
+    $dateFilter .= '</div>'.PHP_EOL;
+    echo GridView::widget([
         'dataProvider' => $dataProvider,
         'filterModel' => $searchModel,
         'columns' => [
@@ -28,6 +40,7 @@ $this->params['breadcrumbs'][] = $this->title;
                 'label' => 'Date',
                 'attribute' => 'date',
                 'format' => 'raw',
+                'filter' => $dateFilter,
                 'value' => function ($data) {
                     return date('j F Y', strtotime($data->date));
                 },
